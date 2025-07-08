a18e063df0ce4aa2445d0b46a4f4f1419a62e239
atulyado <133553524+atulyado@users.noreply.github.com>
2025-07-08 20:35:14 +0700

    Update longest_Subsequence.java

--- longest_Subsequence.java
+++ longest_Subsequence.java
@@ -2,17 +2,17 @@ import java.util.Arrays;
 
 class Solution {
     public int findLHS(int[] nums) {
-        Arrays.sort(nums);
-        int left = 0;
+        Arrays.sort(nums);//sort the nums
+        int left = 0;//this will point to the left side
         int ans = 0;
 
-        for(int right =0 ;right < nums.length;right++){
-            if(nums[right] - nums[left] > 1){
+        for(int right =0 ;right < nums.length;right++){// this will pointer to the right side every turn
+            if(nums[right] - nums[left] > 1){//if the answer is more than one move the left pointer to the right
                 left++;
             }
 
-            if(nums[right] - nums[left] == 1){
-                ans = Math.max(ans, right - left + 1);
+            if(nums[right] - nums[left] == 1){//if the answer is one 
+                ans = Math.max(ans, right - left + 1);//this will take the biggest lenght found and keep upathing it
 
             }
         }
